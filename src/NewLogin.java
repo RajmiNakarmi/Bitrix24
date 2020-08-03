18b98432cf1f095a701a23fa794f27813768598f
RajmiNakarmi <68827515+RajmiNakarmi@users.noreply.github.com>
2020-08-02 23:11:08 -0400

    Update NewLogin.java

--- src/NewLogin.java
+++ src/NewLogin.java
@@ -2,5 +2,6 @@ public class NewLogin {
     public static void main(String[] args) {
         System.out.println("Hello!");
         System.out.println("My first day.");
+        // yay!i think it. its working
     }
 }
